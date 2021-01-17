272d55b1e377b3aa11c035925493aba65f09cbcb
Pau Ruiz Safont <unduthegun@gmail.com>
2021-01-17 18:32:36 +0000

    maintenance: format with ocamlformat

--- lib/xapi-stdext-date/date.mli
+++ lib/xapi-stdext-date/date.mli
@@ -18,30 +18,31 @@
 (** An ISO-8601 date/time type. *)
 type iso8601
 
-(** Convert calendar time [x] (as returned by e.g. Unix.time), to time in UTC. *)
 val of_float : float -> iso8601
+(** Convert calendar time [x] (as returned by e.g. Unix.time), to time in UTC. *)
 
+val to_float : iso8601 -> float
 (** Convert date/time to a float value: the number of seconds since 00:00:00 UTC, 1 Jan 1970.
   * Assumes the underlying iso8601 is in UTC *)
-val to_float : iso8601 -> float
 
-(** Convert date/time to an ISO 8601 formatted string. *)
 val to_string : iso8601 -> string
+(** Convert date/time to an ISO 8601 formatted string. *)
 
+val of_string : string -> iso8601
 (** Convert ISO 8601 formatted string to a date/time value.
   * Does not accept a timezone annotated datetime - i.e. string must be UTC, and end with a Z *)
-val of_string : string -> iso8601
 
+val assert_utc : iso8601 -> unit
+  [@@deprecated
+    "assertions performed inside constructors, so this fn does nothing"]
 (** Raises an Invalid_argument exception if the given date is not a UTC date.
  *  A UTC date is an ISO 8601 strings that ends with the character 'Z'. *)
-val assert_utc : iso8601 -> unit
-[@@deprecated "assertions performed inside constructors, so this fn does nothing"]
 
+val never : iso8601
 (** Representation of the concept "never" (actually 00:00:00 UTC, 1 Jan 1970). *)
-val never: iso8601
 
-(** exposed for testing *)
 val _localtime_string : Ptime.tz_offset_s option -> Ptime.t -> string
+(** exposed for testing *)
 
 val localtime : unit -> iso8601
 
@@ -50,10 +51,10 @@ val localtime : unit -> iso8601
 (** An RFC 822 date/time type. *)
 type rfc822
 
-(** Convert calendar time [x] (as returned by e.g. Unix.time), to RFC 822. *)
 val rfc822_of_float : float -> rfc822
+(** Convert calendar time [x] (as returned by e.g. Unix.time), to RFC 822. *)
 
-(** Convert RFC 822 date/time to a formatted string. *)
 val rfc822_to_string : rfc822 -> string
+(** Convert RFC 822 date/time to a formatted string. *)
 
 val eq : iso8601 -> iso8601 -> bool
--- lib/xapi-stdext-zerocheck/zerocheck.mli
+++ lib/xapi-stdext-zerocheck/zerocheck.mli
@@ -12,30 +12,33 @@
  * GNU Lesser General Public License for more details.
  *)
 
-(** [is_all_zeroes x len] returns true if the substring is all zeroes *)
 external is_all_zeros : string -> int -> bool = "is_all_zeros"
+(** [is_all_zeroes x len] returns true if the substring is all zeroes *)
 
+val find_a_zero : string -> int -> int -> int option
 (** [find_a_zero x len offset] returns the offset in [x] of a zero
     character after [offset], or None if no zero was detected.
     Note this function is approximate and is not guaranteed to find
     strictly the first zero. *)
-val find_a_zero: string -> int -> int -> int option
 
+val find_a_nonzero : string -> int -> int -> int option
 (** [find_a_nonzero x len offset] returns the offset in [x] of a 
     nonzero character after [offset], or None if none could be detected.
     Note this function is approximate and is not guaranteed to find
     strictly the first nonzero. *)
-val find_a_nonzero: string -> int -> int -> int option
 
-type substring = {
-  buf: string;
-  offset: int;
-  len: int
-}
+type substring = {buf: string; offset: int; len: int}
 
+val fold_over_nonzeros :
+     string
+  -> int
+  -> (int -> int)
+  -> (int -> int)
+  -> ('a -> substring -> 'a)
+  -> 'a
+  -> 'a
 (** [fold_over_nonzeros buf len rounddown roundup f initial] folds [f] over all 
     (start, length) pairs of non-zero data in string [buf] up to [len]. 
     	The start of each pair is rounded down with [rounddown] and 
     the end offset of each pair is rounded up with [roundup] (e.g. to 
     potential block boudaries. *)
-val fold_over_nonzeros: string -> int -> (int -> int) -> (int -> int) -> ('a -> substring -> 'a) -> 'a -> 'a
